cbfbfc88209bd29491faee52de705c72e70bfb8c
paul45 <paul.tallet.pinet@free.fr>
2020-01-24 14:40:41 +0100

    Update GroupModel.php

--- src/Authorization/GroupModel.php
+++ src/Authorization/GroupModel.php
@@ -148,4 +148,35 @@ class GroupModel extends Model
             ->where('permission_id', $permissionId)
             ->delete();
     }
+    /**
+     * Gets all permissions for a group in a way that can be
+     * easily used to check against:
+     *
+     * [
+     *  id => name,
+     *  id => name
+     * ]
+     *
+     * @param int $groupId
+     *
+     * @return array
+     */
+    public function getPermissionsForGroup(int $groupId): array
+    {
+        $fromGroup = $this->db->table('auth_groups')
+            ->select('auth_permissions.id, auth_permissions.name, auth_permissions.description')
+            ->join('auth_groups_permissions', 'auth_groups_permissions.group_id = auth_groups.id', 'inner')
+            ->join('auth_permissions', 'auth_permissions.id = auth_groups_permissions.permission_id', 'inner')
+            ->where('auth_groups.id', $groupId)
+            ->get()
+            ->getResultObject();
+
+        $found = [];
+        foreach ($fromGroup as $permission)
+        {
+            $found[$permission->id] = $permission;
+        }
+
+        return $found;
+    }
 }
